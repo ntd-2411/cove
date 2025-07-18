baacd16c1d81fdba32566a9fba24fe02637d1e07
ntd-2411 <nguyentiendung134689@gmail.com>
2025-07-18 19:14:04 +0700

    btvn

--- bai1/src/b1.java
+++ bai1/src/b1.java
@@ -1,11 +1,6 @@
 import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class b1 {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         Scanner sc = new Scanner(System.in);
         System.out.println("nhap ten cua ban: ");
         String ten = sc.nextLine();
@@ -14,6 +9,5 @@ public class b1 {
         System.out.println("nhap chieu cao cua ban: ");
         int cao = sc.nextInt();
         System.out.print(ten + " " + tuoi + " tuoi " + "cao " + cao + " cm");
-
     }
 }
--- bai1/src/b2.java
+++ bai1/src/b2.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class b2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap ten hoc sinh: ");
+        String name = sc.nextLine();
+        System.out.print("nhap tuoi: ");
+        int age = sc.nextInt();
+        sc.nextLine();
+        System.out.print("nhap lop hoc: ");
+        String lophoc = sc.nextLine();
+        System.out.print("nhap gpa: ");
+        float gpa = sc.nextFloat();
+        sc.nextLine();
+        System.out.print("Ten: " + name + " - Tuoi: " + age + " - lop: " + lophoc + " - GPA:  " + gpa);
+    }
 }
